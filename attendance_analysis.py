f3e4cb2a33ef5d4379f80185244df33547e839b2
Michal Baránek <baranekm@Michals-MacBook-Pro.local>
2024-11-18 18:02:43 +0100

    Adding req

--- attendance_analysis.py
+++ attendance_analysis.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import time
-import time
 from datetime import datetime
 import csv
 import requests
